0fb812a775eb77fc510b8490ef29b1c3eadac4ca
Sujan HG <sujanhgs@gmail.com>
2025-09-12 11:24:28 +0530

    CORS Import

--- src/main/java/com/mtd/ecommerce_app/controller/ProductController.java
+++ src/main/java/com/mtd/ecommerce_app/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.mtd.ecommerce_app.entity.Product;
 import com.mtd.ecommerce_app.service.ProductService;
